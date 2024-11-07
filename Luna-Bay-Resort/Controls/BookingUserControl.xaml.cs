e4f1b54fed5032e98a9eee0ff1735f0f821084a5
JAY <jay.arnon.sinahunon12@gmail.com>
2024-11-07 08:31:23 +0800

    booking contents

--- Luna-Bay-Resort/Controls/BookingUserControl.xaml.cs
+++ Luna-Bay-Resort/Controls/BookingUserControl.xaml.cs
@@ -1,28 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Luna_Bay_Resort.Controls
 {
-    /// <summary>
-    /// Interaction logic for BookingUserControl.xaml
-    /// </summary>
     public partial class BookingUserControl : UserControl
     {
+        public ObservableCollection<ListItem> ListItems { get; set; }
+
         public BookingUserControl()
         {
-            InitializeComponent();
+            InitializeComponent(); // Ensures XAML elements are recognized
+            LoadBookingCards();
+            LoadCheckInOutCards();
+
+            // Initialize the collection and add items
+            ListItems = new ObservableCollection<ListItem>
+            {
+                new ListItem { Text = "Reservation 1" },
+                new ListItem { Text = "Reservation 2" },
+                new ListItem { Text = "Reservation 3" }
+            };
+
+            // Bind the collection to the ListView
+            ListViewRight.ItemsSource = ListItems;
         }
+
+        private void LoadBookingCards()
+        {
+            var reservationCards = new List<Booking>
+            {
+                new Booking { ImageSource = new BitmapImage(new Uri(@"C:\Users\JAY\source\repos\Luna-Bay-Resort\Luna-Bay-Resort\Resources\addReservation.png")), ButtonText = "Add Reservation" },
+                new Booking { ImageSource = new BitmapImage(new Uri(@"C:\Users\JAY\source\repos\Luna-Bay-Resort\Luna-Bay-Resort\Resources\editReservation.png")), ButtonText = "Edit Reservation" },
+                new Booking { ImageSource = new BitmapImage(new Uri(@"C:\Users\JAY\source\repos\Luna-Bay-Resort\Luna-Bay-Resort\Resources\viewReservation.png")), ButtonText = "View Reservation" }
+            };
+
+            // Bind the list to the ItemsControl
+            ReservationContainer.ItemsSource = reservationCards;
+        }
+
+        private void LoadCheckInOutCards()
+        {
+            var checkInOutCards = new List<Booking>
+            {
+                new Booking { ImageSource = new BitmapImage(new Uri(@"C:\Users\JAY\source\repos\Luna-Bay-Resort\Luna-Bay-Resort\Resources\check-in.png")), ButtonText = "Check In" },
+                new Booking { ImageSource = new BitmapImage(new Uri(@"C:\Users\JAY\source\repos\Luna-Bay-Resort\Luna-Bay-Resort\Resources\check-out.png")), ButtonText = "Check Out" }
+            };
+
+            // Bind the list to the ItemsControl for Check In and Out
+            CheckInOutContainer.ItemsSource = checkInOutCards;
+        }
+    }
+
+    public class Booking
+    {
+        public BitmapImage ImageSource { get; set; }
+        public string ButtonText { get; set; }
+    }
+
+    public class ListItem
+    {
+        public string Text { get; set; }
     }
 }
---
+++ Luna-Bay-Resort/Controls/RoundedCard.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Luna_Bay_Resort.Controls
+{
+    public partial class RoundedCard : UserControl
+    {
+        public RoundedCard()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(BitmapImage), typeof(RoundedCard), new PropertyMetadata(null));
+
+        public BitmapImage ImageSource
+        {
+            get { return (BitmapImage)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ButtonTextProperty =
+            DependencyProperty.Register("ButtonText", typeof(string), typeof(RoundedCard), new PropertyMetadata(string.Empty));
+
+        public string ButtonText
+        {
+            get { return (string)GetValue(ButtonTextProperty); }
+            set { SetValue(ButtonTextProperty, value); }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Handle button click here
+        }
+    }
+}
