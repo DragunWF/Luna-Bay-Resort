2ee6ebff96c51e44602ec5a13f5fc431353ed5e8
DragunDev <plarisanmarc@gmail.com>
2024-11-09 18:07:39 +0800

    Create helper classes

---
+++ Luna-Bay-Resort-App/Helpers/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luna_Bay_Resort_App.Helpers
+{
+    internal class Utils
+    {
+    }
+}
