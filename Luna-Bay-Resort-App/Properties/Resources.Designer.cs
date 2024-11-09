4485d123295405e85c4a5a9f6593a10e55d12ba0
DragunDev <plarisanmarc@gmail.com>
2024-11-09 19:52:01 +0800

    Fix bug with Dashboard not loading resources

--- Luna-Bay-Resort-App/Properties/Resources.Designer.cs
+++ Luna-Bay-Resort-App/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace LunaBayFinals.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LunaBayFinals.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Luna_Bay_Resort_App.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
