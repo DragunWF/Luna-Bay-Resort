eae70620ad1ba54c6b69d2b41783475974f71da5
DragunDev <plarisanmarc@gmail.com>
2024-11-09 22:15:59 +0800

    Update Utils.cs - Add utility methods (Generate reservation num and format currency)

--- Luna-Bay-Resort-App/Helpers/Utils.cs
+++ Luna-Bay-Resort-App/Helpers/Utils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace Luna_Bay_Resort_App.Helpers
 {
     internal class Utils
     {
+        private static readonly string digits = "0123456789";
+        private static readonly Random random = new Random();
+
+        public static int GenerateReservationNum()
+        {
+            const int idDigitCount = 5;
+            string strNum = "";
+            for (int i = 0; i < idDigitCount; i++)
+            {
+                strNum += digits[random.Next(0, digits.Length)];
+            }
+            int reservationNum = int.Parse(strNum);
+
+            // Takes care of leading zeroes
+            if (reservationNum.ToString().Length < idDigitCount)
+            {
+                return GenerateReservationNum();
+            }
+            return reservationNum;
+        }
+
+        public static string FormatCurrency(int amount)
+        {
+            return $"{amount} PHP";
+        }
     }
 }
