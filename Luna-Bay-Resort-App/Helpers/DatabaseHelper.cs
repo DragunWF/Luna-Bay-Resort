70efd523e38a2d3f75d613c71d6a3030a65d9baa
DragunDev <plarisanmarc@gmail.com>
2024-11-10 22:47:14 +0800

    Update DatabaseHelper.cs - Improve formatting

--- Luna-Bay-Resort-App/Helpers/DatabaseHelper.cs
+++ Luna-Bay-Resort-App/Helpers/DatabaseHelper.cs
@@ -11,30 +11,30 @@ namespace Luna_Bay_Resort_App.Helpers
     {
         public static SqlConnection con = new SqlConnection("Connection Key");
 
-        public static void AddReservation(String Name, String Email, String Phone, String Room, int NumofGuest, String CheckIn, String CheckOut)
+        public static void AddReservation(string name, string email, string phone, string room, int numOfGuest, string checkIn, string checkOut)
         {
             //temporary for testing
             int Receipt = 10;
             int Reservation = 10;
 
             con.Open();
-            SqlCommand add = new SqlCommand("Insert into Guest(Receipt_No, Reservation_ID, Name, Email, Phone, Room, NumofGuest, Check_in, Check_out, Status, Bill_Amount, Balance) Values(@ReceiptNo, @ReservationID, @Name, @Email, @Phone, @Room, @NumofGuest,@CheckIn, @CheckOut, @Status, @BillAmount, @Balance)", con);
+            SqlCommand add = new SqlCommand("INSERT INTO Guest(Receipt_No, Reservation_ID, Name, Email, Phone, Room, NumofGuest, Check_in, Check_out, Status, Bill_Amount, Balance) Values(@ReceiptNo, @ReservationID, @Name, @Email, @Phone, @Room, @NumofGuest,@CheckIn, @CheckOut, @Status, @BillAmount, @Balance)", con);
             add.Parameters.AddWithValue("@ReceiptNo", Receipt);
             add.Parameters.AddWithValue("@ReservationID", Reservation);
-            add.Parameters.AddWithValue("@Name", Name);
-            add.Parameters.AddWithValue("@Email", Email);
-            add.Parameters.AddWithValue("@Phone", Phone);
-            add.Parameters.AddWithValue("@Room", Room);
-            add.Parameters.AddWithValue("@NumofGuest", NumofGuest);
-            add.Parameters.AddWithValue("@CheckIn", DateTime.Parse(CheckIn));
-            add.Parameters.AddWithValue("@CheckOut", DateTime.Parse(CheckOut));
+            add.Parameters.AddWithValue("@Name", name);
+            add.Parameters.AddWithValue("@Email", email);
+            add.Parameters.AddWithValue("@Phone", phone);
+            add.Parameters.AddWithValue("@Room", room);
+            add.Parameters.AddWithValue("@NumofGuest", numOfGuest);
+            add.Parameters.AddWithValue("@CheckIn", DateTime.Parse(checkIn));
+            add.Parameters.AddWithValue("@CheckOut", DateTime.Parse(checkOut));
             add.Parameters.AddWithValue("@Status", "Reserved");
             add.Parameters.AddWithValue("@BillAmount", 2000); //placeholder
             add.Parameters.AddWithValue("@Balance", 4000); //placeholder
             add.ExecuteNonQuery();
             con.Close();
-            Receipt = +1;
-            Reservation = +1;    
+            Receipt += 1;
+            Reservation += 1;    
 
         }
     }
