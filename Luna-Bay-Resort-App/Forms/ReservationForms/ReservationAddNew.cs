d25c8dfe210f8e05edfe80209cc72e24115da07b
DragunDev <plarisanmarc@gmail.com>
2024-11-10 22:55:52 +0800

    Update ReservationAddNew.cs - Add error handling and improve code formatting

--- Luna-Bay-Resort-App/Forms/ReservationForms/ReservationAddNew.cs
+++ Luna-Bay-Resort-App/Forms/ReservationForms/ReservationAddNew.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Luna_Bay_Sub_Forms
 {
@@ -22,8 +23,51 @@ namespace Luna_Bay_Sub_Forms
 
         private void ConfirmButton_Click(object sender, EventArgs e)
         {
-            DatabaseHelper.AddReservation(FirstNameText.Text + "" + LastNameText.Text, EmailText.Text, ContactNoText.Text, RoomTypeText.Text, int.Parse(GuestNumText.Text), CheckInPicker.Text, CheckOutPicker.Text);
-            MessageBox.Show("Success");
+            try
+            {
+                string fullName = $"{FirstNameText.Text} {LastNameText.Text}";
+                if (IsValidTextBoxes(new string[] {
+                    fullName,
+                    EmailText.Text,
+                    ContactNoText.Text,
+                    RoomTypeText.Text,
+                    GuestNumText.Text,
+                    CheckInPicker.Text,
+                    CheckOutPicker.Text
+                }))
+                {
+                    DatabaseHelper.AddReservation(
+                        fullName,
+                        EmailText.Text,
+                        ContactNoText.Text,
+                        RoomTypeText.Text,
+                        int.Parse(GuestNumText.Text),
+                        CheckInPicker.Text,
+                        CheckOutPicker.Text
+                    );
+                    MessageBox.Show("Success");
+                }
+                else
+                {
+                    MessageBox.Show("Please don't leave any text boxes empty!");
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"An unexpected error has occured: {err.Message}");
+            }
+        }
+
+        private bool IsValidTextBoxes(string[] textValues)
+        {
+            foreach (string value in textValues)
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
