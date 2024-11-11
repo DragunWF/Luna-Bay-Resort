fec7577d1852f8814e3a0eabb20d5666a52cd0d4
DragunDev <plarisanmarc@gmail.com>
2024-11-11 18:40:43 +0800

    Create User.cs - data class

---
+++ Luna-Bay-Resort-App/Data/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luna_Bay_Resort_App.Data
+{
+    internal class User
+    {
+        private string empId, position, name;
+        private int authId;
+
+        public User(string empId, string position, string name, int authId)
+        {
+            this.empId = empId;
+            this.position = position;
+            this.name = name;
+            this.authId = authId;
+        }
+
+        #region Getter Methods
+
+        public string getEmpId() => empId;
+        public string getPosition() => position;
+        public string getName() => name;
+        public int getAuthId() => authId;
+
+        #endregion
+    }
+}
