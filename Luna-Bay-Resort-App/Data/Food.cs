3fce7e7735b6ec14a6bf6fb2d3f57fd71c5c0d25
Aysaaak <isaac.6severino@gmail.com>
2024-11-12 16:44:27 +0800

    Create Food.cs - data class

---
+++ Luna-Bay-Resort-App/Data/Food.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luna_Bay_Resort_App.Data
+{
+    internal class Food
+    {
+        private int foodId, foodTypeId, price;
+        private string foodName, serving;
+
+        public Food(int foodId, int foodType, string foodName, string serving, int price)
+        {
+            this.foodId = foodId;
+            this.foodTypeId = foodType;
+            this.foodName = foodName;
+            this.serving = serving;
+            this.price = price;
+        }
+
+        #region Getter Methods
+        public int getFoodId() => foodId;
+        public int getFoodType() => foodTypeId;
+        public string getFoodName() => foodName;
+        public string getServing() => serving;
+        public int getPrice() => price;
+        #endregion
+    }
+}
