3fb1625596181d6f123797977f3c6c2aafadedee
DragunDev <plarisanmarc@gmail.com>
2024-11-24 20:12:27 +0800

    Create reset.sql - For resetting the database without dropping it

---
+++ Database/reset.sql
@@ -0,0 +1,41 @@
+USE LunaBayResortDB;
+
+-- Drop tables in the correct order to avoid dependency issues
+IF OBJECT_ID('Bookings', 'U') IS NOT NULL
+    DROP TABLE Bookings;
+
+IF OBJECT_ID('Guest', 'U') IS NOT NULL
+    DROP TABLE Guests;
+
+IF OBJECT_ID('Payments', 'U') IS NOT NULL
+    DROP TABLE Payments;
+
+IF OBJECT_ID('Employees', 'U') IS NOT NULL
+    DROP TABLE Employees;
+
+IF OBJECT_ID('FoodType', 'U') IS NOT NULL
+    DROP TABLE FoodType;
+
+IF OBJECT_ID('Food', 'U') IS NOT NULL
+    DROP TABLE Food;
+
+IF OBJECT_ID('Products', 'U') IS NOT NULL
+    DROP TABLE Products;
+
+IF OBJECT_ID('PaymentMethod', 'U') IS NOT NULL
+    DROP TABLE PaymentMethod;
+
+IF OBJECT_ID('POSReceipt', 'U') IS NOT NULL
+    DROP TABLE POSReceipt;
+
+IF OBJECT_ID('Revenue', 'U') IS NOT NULL
+    DROP TABLE Revenue;
+
+IF OBJECT_ID('Activities', 'U') IS NOT NULL
+    DROP TABLE Activities;
+
+IF OBJECT_ID('Accommodation', 'U') IS NOT NULL
+    DROP TABLE Accommodation;
+
+IF OBJECT_ID('RoomType', 'U') IS NOT NULL
+    DROP TABLE RoomType;
