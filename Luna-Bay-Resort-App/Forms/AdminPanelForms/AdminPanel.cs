6d50bd9bb289a3ed68e35e65c4ea4d2d10d604b4
DragunDev <plarisanmarc@gmail.com>
2024-11-25 21:11:43 +0800

    Update admin panel

--- Luna-Bay-Resort-App/Forms/AdminPanelForms/AdminPanel.cs
+++ Luna-Bay-Resort-App/Forms/AdminPanelForms/AdminPanel.cs
@@ -1,10 +1,32 @@
-﻿namespace Luna_Bay_Resort_App.Forms.AdminPanelForms
+﻿using Luna_Bay_Resort_App.Helpers;
+
+namespace Luna_Bay_Resort_App.Forms.AdminPanelForms
 {
     public partial class AdminPanel : Form
     {
         public AdminPanel()
         {
             InitializeComponent();
+        }
+
+        private void AddAccountBtn_Click(object sender, EventArgs e)
+        {
+            FormManager.OpenForm<AddAccountForm>();
+        }
+
+        private void ViewAccountsBtn_Click(object sender, EventArgs e)
+        {
+            FormManager.OpenForm<ViewAccountsForm>();
+        }
+
+        private void ResetAccountBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteAccountBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
