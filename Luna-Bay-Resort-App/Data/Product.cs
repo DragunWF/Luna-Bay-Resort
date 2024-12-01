185dee6d012d86124e83141b40e84b1e68d14ed7
DragunDev <plarisanmarc@gmail.com>
2024-12-01 23:00:03 +0800

    Update Product.cs

--- Luna-Bay-Resort-App/Data/Product.cs
+++ Luna-Bay-Resort-App/Data/Product.cs
@@ -2,13 +2,13 @@
 {
     internal class Product
     {
-        private int product_Id, stock;
+        private int productId, stock;
         private double price;
         private string name;
 
-        public Product(int product_Id, string name, double price, int stock)
+        public Product(int productId, string name, double price, int stock)
         {
-            this.product_Id = product_Id;
+            this.productId = productId;
             this.name = name;
             this.price = price;
             this.stock = stock;
@@ -23,7 +23,7 @@
 
         #region Getter Methods
 
-        public int GetProductId() => product_Id;
+        public int GetProductId() => productId;
         public string GetProductName() => name;
         public double GetPrice() => price;
         public int GetStock() => stock;
